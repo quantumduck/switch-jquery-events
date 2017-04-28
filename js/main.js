79da2a19a96dca10631f9ec137fed74aa7bbd851
quantumduck <quantum.duck@hotmail.com>
2017-04-28 11:46:09 -0400

    Changed status message and finished!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(function() {
-
+  var status = $('.status').first();
+  var originalText = status.text();
   flashMessage('Ready to go!');
   var lightSwitch = $('.switch').first();
   lightSwitch.on('click', function() {
     // window.alert('The switch was clicked!')
+    //1: Change classes on lightSwitch
     if (lightSwitch.hasClass('on')) {
       lightSwitch.removeClass('on');
       lightSwitch.addClass('off');
@@ -11,9 +13,14 @@ $(function() {
       lightSwitch.removeClass('off');
       lightSwitch.addClass('on');
     }
-
+    //2: Toggle body class:
     $('body').toggleClass('dark', 'light');
-
+    //3: Change the message:
+    if (status.text() === originalText) {
+      status.text("Hey! Who turned off the lights?");
+    } else {
+      status.text(originalText);
+    }
   });
 
 
