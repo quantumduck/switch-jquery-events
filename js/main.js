f39b22bd364c769c6c8f0194e928d6fb6a12c51f
quantumduck <quantum.duck@hotmail.com>
2017-04-28 10:57:06 -0400

    adding temporary message once JS loaded

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  var status = $('.status').first();
+  var originalText = status.text();
+  status.text('Ready to go!');
 
-
+  setTimeout(function() {
+    $('.status').first().text(originalText);
+  }, 500);
 });
+
+function throwTheSwitch() {
+
+}
